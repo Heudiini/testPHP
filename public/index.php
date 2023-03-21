996e086665e15d41038e1cfdb001665206246a42
Heudiini <77001762+Heudiini@users.noreply.github.com>
2023-03-21 14:36:19 +0000

    testing

--- public/index.php
+++ public/index.php
@@ -7,3 +7,13 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+// ...
+$kernel = new Kernel($environment, $debug);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+
+// Add this line
+$response = (new App\Controller\DefaultController())->index();
+
+$response->send();
+$kernel->terminate($request, $response);
