53f931d35f23ab3b59c47d61e38347fd8be1aac0
Heudiini <77001762+Heudiini@users.noreply.github.com>
2023-03-21 14:53:56 +0000

    bundle

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+# List of directories that contain PHP files
+DIRS=("src/" "public/")
 
 # Build script for serving static content instead of php files
 DEST="./public_html"
@@ -8,27 +9,26 @@ html=".html"
 mkdir -p "$DEST/"
 
 # Execute all php files and save them as html
-for f in *.php; 
-do
-    php $f | sed 's:\(<a.*href=".*\)\.php\(".*</a>\):\1\.html\2:g' > "$DEST/${f/.php/$html}";
-    echo "Processing $f into ${f/.php/$html}..";
+for dir in "${DIRS[@]}"; do
+    for f in "$dir"*.php; do
+        php "$f" | sed 's:\(<a.*href=".*\)\.php\(".*</a>\):\1\.html\2:g' > "$DEST/${f/$dir/$html}";
+        echo "Processing $f into ${f/$dir/$html}..";
+    done
 done
 
-
-#Copy all CSS files
-for f in *.css; 
-do
-    cat $f > "$DEST/$f";
+# Copy all CSS files
+for f in *.css; do
+    cat "$f" > "$DEST/$f";
     echo "Processing $f file..";
 done
 
-#Copy styles.css
+# Copy styles.css
 cp public/styles.css "$DEST/styles.css";
 echo "Processing styles.css file..";
-#Copy all JS files
-for f in *.js; 
-do
-    cat $f > "$DEST/$f";
+
+# Copy all JS files
+for f in *.js; do
+    cat "$f" > "$DEST/$f";
     echo "Processing $f file..";
 done
 
---
+++ public_html/.htmlKernel.php
@@ -0,0 +1,5 @@
+
+Fatal error: Uncaught Error: Class "Symfony\Component\HttpKernel\Kernel" not found in /Users/heidimorsky/Desktop/phpuusi/testPHP/src/Kernel.php:8
+Stack trace:
+#0 {main}
+  thrown in /Users/heidimorsky/Desktop/phpuusi/testPHP/src/Kernel.php on line 8
